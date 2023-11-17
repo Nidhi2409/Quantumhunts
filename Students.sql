1f25eb156584c7b4ce2ca98ecdcce3787ad24638
Nidhi2409 <81637011+Nidhi2409@users.noreply.github.com>
2023-11-17 11:31:23 +0530

    Add files via upload

---
+++ Students.sql
@@ -0,0 +1,5 @@
+CREATE TABLE students (
+    class VARCHAR(50),
+    name_of_student VARCHAR(50),
+    sort_rank INT
+);
---
+++ insert_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO students (class, name_of_student, sort_rank) VALUES ('Class A', 'Arun', 1);
+INSERT INTO students (class, name_of_student, sort_rank) VALUES ('Class B', 'Tejas', 1);
+INSERT INTO students (class, name_of_student, sort_rank) VALUES ('Class B', 'David', 2);
+INSERT INTO students (class, name_of_student, sort_rank) VALUES ('Class A', 'Uma', 2);
+INSERT INTO students (class, name_of_student, sort_rank) VALUES ('Class B', 'Divya', 3);
+INSERT INTO students (class, name_of_student, sort_rank) VALUES ('Class A', 'Simon', 3);
+INSERT INTO students (class, name_of_student, sort_rank) VALUES ('Class A', 'Rakesh', 4);
+INSERT INTO students (class, name_of_student, sort_rank) VALUES ('Class B', 'Selvam', 4);
+INSERT INTO students (class, name_of_student, sort_rank) VALUES ('Class B', 'Shruthi', 5);
+INSERT INTO students (class, name_of_student, sort_rank) VALUES ('Class A', 'Deepak', 5);
